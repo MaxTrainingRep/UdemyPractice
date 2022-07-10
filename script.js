3c52c9969ecd3d019b62cd4760a9e7b576304d97
MaxTrainingRep <mxgryn@gmail.com>
2022-07-10 11:16:03 +0300

    add test for count of movies

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных Вами фильмов', 'Название фильма'),
           b = +prompt('Оцените этот фильм');
@@ -16,6 +17,16 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 };
+
+if (personalMovieDB.count < 10) {
+    console.log('litle');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log(' norm');
+} else if (personalMovieDB.count > 30) {
+    console.log(`too much`)
+} else {
+    console.log(' errrrroooorrr')
+}
         
 
 console.log(personalMovieDB)
